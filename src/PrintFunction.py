5b0e075cc4d016d7e81feb9be2410bf44390d9c8
Ema Fazillah binti Abu Bakar <ema.fazillah.abu@rhbgroup.com>
2018-12-31 17:37:18 +0800

    Print function

---
+++ src/PrintFunction.py
@@ -0,0 +1,11 @@
+'''
+Created on 31 Dec 2018
+
+@author: emafazillah
+'''
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(1, n + 1):
+        print(i, sep='', end='')
